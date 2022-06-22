494dccbba1a063c1a0788404e3607bb9d0eebf8d
luana maldonado <51408179+luanadacosta@users.noreply.github.com>
2022-06-21 22:28:37 -0300

    java

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 //criar uma função em JavaScript
 //formato ES6 baseado em arrow function
+
+//função que realiza o cadastro de livro alert('Ok') 
 const cadastrar = () =>{
-    alert('Ok') 
+   
+    //captura o valor digitado no titulo
+    let titulo = 'Senhor dos Aneis' //valor fixo
+
+    alert(titulo) //msg de texto colocar (titulo) , ('titulo') string
 }
 
 
